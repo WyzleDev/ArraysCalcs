3b600ea84157c751b5eeb0739d468a2aefb89c67
WyzleDev <codename.wyzle@gmail.com>
2022-05-11 14:47:30 +0300

    Add project files.

---
+++ ArraysCalcs/ArrayCalcs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysCalcs
+{
+    public class ArrayCalcs
+    {
+
+        static public List<float> multiply(List<float> lst1, List<float> lst2)
+        {
+            List<float> result = new List<float>();
+
+            for (int i = 0; i <= lst1.Count-1; i++)
+            {
+                for (int j = 0; j <= lst2.Count-1; j++)
+                {
+                    result.Add(lst1[i] * lst2[j]);
+                }
+            }
+            for (int i = 0; i <= result.Count-1; i++)
+            { 
+                Console.Write(result[i] + " ");
+            }
+            Console.WriteLine("//Massive multiplied by other massive");
+            return result;
+
+        }
+        static public List<float> multiply_by_number(List<float> lst, float n)
+        {
+            Console.WriteLine();
+            List<float> result = new List<float>();
+
+            for (int i = 0; i <= lst.Count - 1; i++)
+            {
+                result.Add(lst[i]*n);
+            }
+            for (int i = 0; i <= result.Count - 1; i++)
+            {
+                
+                Console.Write(result[i] + " ");
+            }
+            Console.WriteLine("//Massive multiplied by number");
+            return result;
+        }
+        public static List<List<T>> transpose<T>(List<List<T>> lists)
+        {
+            var longest = lists.Any() ? lists.Max(l => l.Count) : 0;
+            List<List<T>> outer = new List<List<T>>(longest);
+            for (int i = 0; i < longest; i++)
+                outer.Add(new List<T>(lists.Count));
+            for (int j = 0; j < lists.Count; j++)
+                for (int i = 0; i < longest; i++)
+                    outer[i].Add(lists[j].Count > i ? lists[j][i] : default(T))
+            return outer;
+        }
+
+    }
+}
---
+++ ArraysCalcs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ArraysCalcs
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<float> lst1 = new List<float>();
+            List<float> lst2 = new List<float>();
+            for (int i = 0; i <= 4; i++)
+            {
+                Random rnd = new Random();
+                float random = rnd.Next(1, 10);
+                lst1.Add(random);
+            }
+            for (int i = 0; i <= 4; i++)
+            {
+                Random rnd = new Random();
+                float random = rnd.Next(1, 10);
+                lst2.Add(random);
+            }
+
+            List<List<float>> total = new List<List<float>>();
+            total.Add(lst1);
+            total.Add(lst2);
+
+            ArrayCalcs.multiply(lst1, lst2);
+            ArrayCalcs.multiply_by_number(lst1, 2);
+            ArrayCalcs.transpose(total);
+
+        }
+    } 
+   
+
+}
