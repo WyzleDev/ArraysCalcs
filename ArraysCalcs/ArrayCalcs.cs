9c74a9421a2b6de9e0a5ee1644fc3a82904e13d5
WyzleDev <codename.wyzle@gmail.com>
2022-05-11 16:51:26 +0300

    added a comments for code

--- ArraysCalcs/ArrayCalcs.cs
+++ ArraysCalcs/ArrayCalcs.cs
@@ -8,19 +8,21 @@ namespace ArraysCalcs
 {
     public class ArrayCalcs
     {
-
+        //function that multiply two arrays 
         static public List<float> multiply(List<float> lst1, List<float> lst2)
         {
+            //creating a result list
             List<float> result = new List<float>();
 
-            for (int i = 0; i <= lst1.Count-1; i++)
+            //iterate over the values of two arrays
+            for (int i = 0; i <= lst1.Count-1; i++) //iteraiting over first array
             {
-                for (int j = 0; j <= lst2.Count-1; j++)
+                for (int j = 0; j <= lst2.Count-1; j++) //iteraiting over second array
                 {
-                    result.Add(lst1[i] * lst2[j]);
+                    result.Add(lst1[i] * lst2[j]); //Multiply item of first array by item of second array
                 }
             }
-            for (int i = 0; i <= result.Count-1; i++)
+            for (int i = 0; i <= result.Count-1; i++) //cycle that return string with items of lists
             { 
                 Console.Write(result[i] + " ");
             }
@@ -28,15 +30,21 @@ namespace ArraysCalcs
             return result;
 
         }
+        //function that mutliplys array by number
         static public List<float> multiply_by_number(List<float> lst, float n)
         {
-            Console.WriteLine();
+            Console.WriteLine(); // no comments :D
+
+            //creating a result list
             List<float> result = new List<float>();
 
+            //iteraiting over array and multiply his values by number
             for (int i = 0; i <= lst.Count - 1; i++)
             {
                 result.Add(lst[i]*n);
             }
+
+            //cycle that return string with items of lists
             for (int i = 0; i <= result.Count - 1; i++)
             {
                 
@@ -45,6 +53,7 @@ namespace ArraysCalcs
             Console.WriteLine("//Massive multiplied by number");
             return result;
         }
+        //Function that transpose arrays
         public static List<List<T>> transpose<T>(List<List<T>> lists)
         {
             var longest = lists.Any() ? lists.Max(l => l.Count) : 0;
@@ -53,7 +62,7 @@ namespace ArraysCalcs
                 outer.Add(new List<T>(lists.Count));
             for (int j = 0; j < lists.Count; j++)
                 for (int i = 0; i < longest; i++)
-                    outer[i].Add(lists[j].Count > i ? lists[j][i] : default(T))
+                    outer[i].Add(lists[j].Count > i ? lists[j][i] : default(T));
             return outer;
         }
 
--- ArraysCalcs/Program.cs
+++ ArraysCalcs/Program.cs
@@ -6,8 +6,11 @@ namespace ArraysCalcs
     {
         static void Main()
         {
+            //empty arrays for examples
             List<float> lst1 = new List<float>();
             List<float> lst2 = new List<float>();
+
+            //fill arrays by random number
             for (int i = 0; i <= 4; i++)
             {
                 Random rnd = new Random();
@@ -20,11 +23,13 @@ namespace ArraysCalcs
                 float random = rnd.Next(1, 10);
                 lst2.Add(random);
             }
-
+            
+            //creating list of lists for transpose() func
             List<List<float>> total = new List<List<float>>();
             total.Add(lst1);
             total.Add(lst2);
 
+            //calling all functions
             ArrayCalcs.multiply(lst1, lst2);
             ArrayCalcs.multiply_by_number(lst1, 2);
             ArrayCalcs.transpose(total);
